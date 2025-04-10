5f74972b9dd0bdb4eb3c0ef35701f3f4a4f82d31
NikolayIgnatev <114672912+NikolayIgnatev@users.noreply.github.com>
2025-04-10 15:47:44 +0300

    code structure changed

--- pythonProject7/main.py
+++ pythonProject7/main.py
@@ -1,44 +1,43 @@
-import ptbot
 import os
+import ptbot
 from dotenv import load_dotenv
 from pytimeparse import parse
+
+load_dotenv()
+
+TG_TOKEN = os.environ['TELEGRAM_TOKEN']
+TG_CHAT_ID = os.environ['TG_CHAT_ID']
+bot = ptbot.Bot(TG_TOKEN)
 
 
 def reply(chat_id, text):
     time = parse(text)
-    message_id = bot.send_message(chat_id, "Осталось {} секунд!".format(time))
-    bot.create_countdown(time, notify, chat_id=chat_id,
-                         message_id=message_id, total=time)
+    message_id = bot.send_message(chat_id, f"Осталось {time} секунд!")
+    bot.create_countdown(time, notify, chat_id=chat_id, message_id=message_id, total=time)
     bot.create_timer(time + 1, seconds_left, chat_id=chat_id)
 
 
 def notify(time, chat_id, message_id, total):
-    bot.update_message(chat_id, message_id,
-                       f"{render_progressbar(total,time)}\nОсталось {time} секунд!")
+    bot.update_message(chat_id, message_id, f"{render_progressbar(total, time)}\nОсталось {time} секунд!")
 
 
 def seconds_left(chat_id):
-    bot.send_message(chat_id, 'время вышло')
+    bot.send_message(chat_id, "время вышло")
 
 
-def render_progressbar(total, iteration, prefix='',
-                       suffix='', length=30, fill='█', zfill='░'):
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    return f'{prefix} |{pbar}| {percent}% {suffix}'
 
+
+def main():
+    bot.send_message(TG_CHAT_ID, "Бот запущен")
+    bot.reply_on_message(reply)
+    bot.run_bot()
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    TG_TOKEN = os.environ['TELEGRAM_TOKEN']
-    TG_CHAT_ID = os.environ['TG_CHAT_ID']
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.send_message(TG_CHAT_ID, "Бот запущен")
-
-    bot = ptbot.Bot(TG_TOKEN)
-    bot.reply_on_message(reply)
-    bot.run_bot()
+    main()
